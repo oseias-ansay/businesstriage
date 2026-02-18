eb34574303f0a8d5c27d91524ed65a4f1f8c8e77
OSEIAS SOARES ANSAY <86591155+oseias-ansay@users.noreply.github.com>
2026-02-18 18:21:52 -0300

    correção App.tsx

--- ignitegym/mobile/App.tsx
+++ ignitegym/mobile/App.tsx
@@ -1,27 +1,28 @@
-/* eslint-disable camelcase */
-import { StatusBar } from 'react-native'
-import {
-  useFonts,
-  Roboto_700Bold,
-  Roboto_400Regular,
-} from '@expo-google-fonts/roboto'
-import { GluestackUIProvider } from '@gluestack-ui/themed'
-import { config } from './config/gluestack-ui.config'
-import { Loading } from './src/compenents/Loading'
-import { Routes } from '@routes/index'
+import { StatusBar } from 'react-native';
+import {GluestackUIProvider} from '@gluestack-ui/themed';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Routes } from '@routes/index';
+
+import { AuthContextProvider } from './src/context//authContext';
+
+import { config } from '@gluestack-ui/config';
+
+import { Loading } from './src/compenents/Loading';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <GluestackUIProvider config={config}>
-      <StatusBar
+      <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-
-      {fontsLoaded ? <Routes /> : <Loading />}
+      <AuthContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContextProvider>
     </GluestackUIProvider>
-  )
+  );
 }
